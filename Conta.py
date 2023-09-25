cbf76d7cd231f0902ed1d086eab53f4548ef76a5
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-09-25 08:20:57 -0400

    'Conta' structured

--- Conta.py
+++ Conta.py
@@ -1,1 +1,18 @@
 class Conta:
+    saldo: float
+    numero: int
+    agencia: str
+    # cliente: Cliente
+    # historico: Historico
+
+    def saldo(self) -> float:
+        pass
+
+    # def nova_conta(self, cliente: Cliente, numero: int) -> Conta:
+    #     pass
+
+    def sacar(self, valor: float) -> bool:
+        pass
+
+    def depositar(self, valor: float) -> bool:
+        pass
