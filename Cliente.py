9a1449f664ebbb528e45ed32cb73fdc3128a9ef2
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-09-28 11:01:29 -0400

    Classe cliente adicionada

---
+++ Cliente.py
@@ -0,0 +1,17 @@
+from Conta import Conta
+from Transacao import Transacao
+
+
+class Cliente:
+    endereco: str
+    contas: list
+
+    def __init__(self, endereco: str):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta: Conta, transacao: Transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta: Conta):
+        self.contas.append(conta)
