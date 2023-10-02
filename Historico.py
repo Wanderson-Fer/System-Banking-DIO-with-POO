d18dc4f86a2da565b1191deecd84a2463179f643
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-10-02 09:30:59 -0400

    Classe historico modificada

--- Historico.py
+++ Historico.py
@@ -1,6 +1,23 @@
+from datetime import datetime
 from Transacao import Transacao
 
 
 class Historico:
+    _transacao: list
+
+    def __init__(self):
+        self._transacao = []
+
+    @property
+    def transacoes(self) -> list:
+        return self._transacao
+
     def adicionar_transacao(self, transacao: Transacao):
-        pass
+        self._transacao.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().
+                strftime("%d-%m-%Y %H:%M:%s")
+            }
+        )
